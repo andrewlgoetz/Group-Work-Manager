09d575911e0750aa077a13ed6c1a6b99ee461296
andrewlgoetz <100867543+andrewlgoetz@users.noreply.github.com>
2024-01-04 16:31:05 -0500

    USER AUTH APP TEMPLATE

--- app/dash/routes.py
+++ app/dash/routes.py
@@ -3,11 +3,13 @@ from imports import *
 
 dash_bp = Blueprint("dash_bp", __name__, template_folder='templates', static_folder = 'static')
 
+
+
 #index
 
 @dash_bp.route("/")
 def index():
-    # new = Users(username = "bob" )
-    # db.session.add(new)
-    # db.session.commit()
+
+
+
     return render_template("index.html")
