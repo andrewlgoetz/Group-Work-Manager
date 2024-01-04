aaf2541e107be7d64176492c3efc2200e1d217b6
andrewlgoetz <100867543+andrewlgoetz@users.noreply.github.com>
2024-01-04 02:03:52 -0500

    added navbar and some html structure

--- app/Auth/routes.py
+++ app/Auth/routes.py
@@ -12,5 +12,6 @@ auth_bp = Blueprint("auth_bp", __name__, template_folder='templates', static_fol
 
 @auth_bp.route("/new_user")
 def new_user():
-    user = Users.query.filter_by(username="bob").first()
-    return render_template("new_user.html", id = user.id)
+    #user = Users.query.filter_by(username="bob").first()
+    # return render_template("new_user.html", id = user.id)
+    return render_template("new_user.html", id = 99)
