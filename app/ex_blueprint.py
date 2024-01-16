1749ee6d3eff03ddc0797ccc773f3b1108469de2
andrewlgoetz <100867543+andrewlgoetz@users.noreply.github.com>
2024-01-15 19:57:08 -0500

    Delete ex_blueprint.py

--- app/ex_blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-example_blueprint = Blueprint('example_blueprint', __name__)
-
-
-@example_blueprint.route('/home')
-def home():
-    return "This is an example app - blueprint displayed"
